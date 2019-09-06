761c8ff1960af0f242f8653fb1ae959570f1d3e3
Aaron Stannard <aaron@petabridge.com>
2019-09-06 17:58:15 -0500

    working on prototype

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string _originalTitle;
+        private static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        private static void SetTitle(string newTitle)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(newTitle);
+            if (IsWindows) // changing console title is not supported on OS X or Linux
+            {
+                _originalTitle = Console.Title;
+                Console.Title = newTitle;
+            }
+        }
+
+        private static void ResetTitle()
+        {
+            if (IsWindows)
+                Console.Title = _originalTitle; // reset the console window title back
+        }
+
+        static async Task<int> Main(string[] args)
+        {
+            ServerOptions s = null;
+            var r = Parser.Default.ParseArguments<ServerOptions>(args).MapResult(r =>
+            {
+                s = r;
+                return 0;
+            }, _ => 1);
+
+            if (r != 0)
+                return r;
+
+            SetTitle($"System.IO.Pipelines.Server[{s.Host}:{s.Port}]");
+            try
+            {
+                return await RunServer(s);
+            }
+            finally
+            {
+                ResetTitle();
+            }
+
+        }
+
+        private static async Task<int> RunServer(ServerOptions s)
+        {
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp) { Blocking = false };
+            socket.Bind(new DnsEndPoint(s.Host, s.Port));
         }
     }
 }
---
+++ src/Server/ServerOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Server
+{
+    public sealed class ServerOptions
+    {
+        [Option('p', "port", Required = false, HelpText = "Port number to host server on. Defaults to 9000.", Default = 9000)]
+        public int Port { get; set; }
+
+        [Option('h', "hostname", Required = false, HelpText = "Host name to host server on. Defaults to 'localhost'.", Default = "localhost")]
+        public string Host { get; set; }
+    }
+}
