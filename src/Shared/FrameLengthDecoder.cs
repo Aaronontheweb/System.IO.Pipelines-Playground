c22c6c8a2de761b115870a3313c473e2de04ae6a
Aaron Stannard <aaron@petabridge.com>
2019-09-08 11:36:49 -0500

    finished first pass at decoder

--- src/Shared/FrameLengthDecoder.cs
+++ src/Shared/FrameLengthDecoder.cs
@@ -33,32 +33,49 @@ namespace Shared
         public SequencePosition Decode(ReadOnlySequence<byte> input, out IEnumerable<ReadOnlySequence<byte>> msgs)
         {
             var buffer = input;
-            msgs = Enumerable.Empty<ReadOnlySequence<byte>>(); // so we can exit if there are no processed messages
-            SequencePosition? position = null;
+            var decoded = new List<ReadOnlySequence<byte>>();
+            msgs = decoded; // so we can exit if there are no processed messages
+            SequencePosition position = buffer.Start;
 
-            // check to see if we're already discarding an illegally large frame
-            if (discardingTooLongFrame)
+            while(true)
             {
-                var localBytesToDiscard = Math.Min(buffer.Length, bytesToDiscard);
-                position = buffer.GetPosition(localBytesToDiscard);
-                buffer = buffer.Slice(position.Value); // advance buffer for further reading (possibly)
-                bytesToDiscard -= localBytesToDiscard;
-                if (bytesToDiscard == 0)
+                // check to see if we're already discarding an illegally large frame
+                if (discardingTooLongFrame)
                 {
-                    discardingTooLongFrame = false;
-                    _logger.LogDebug("No longer discarding too long frame of size [{0}] bytes.", tooLongFrameLength);
-                    tooLongFrameLength = 0;
-                }
-                else
-                {
-                    
-                    return position.Value; // can't do any further processing - still have more to discard
-                }
-            }
+                    var localBytesToDiscard = Math.Min(buffer.Length, bytesToDiscard);
+                    position = buffer.GetPosition(localBytesToDiscard);
+                    buffer = buffer.Slice(position); // advance buffer for further reading (possibly)
+                    bytesToDiscard -= localBytesToDiscard;
+                    if (bytesToDiscard == 0)
+                    {
+                        discardingTooLongFrame = false;
+                        _logger.LogDebug("No longer discarding too long frame of size [{0}] bytes.", tooLongFrameLength);
+                        tooLongFrameLength = 0;
+                    }
+                    else
+                    {
 
-            if (buffer.Length < frameLengthHeaderSize) // edge case - frame is less than 4bytes
-            {
-                return buffer.Start; // didn't read anything past start
+                        return position; // can't do any further processing - still have more to discard upon next read
+                    }
+                }
+
+                if (buffer.Length < frameLengthHeaderSize) // edge case - frame is less than 4bytes
+                {
+                    return position; // didn't read anything past start
+                }
+
+                var frameLength = Convert.ToInt64(buffer.Slice(frameLengthHeaderSize));
+                if (buffer.Length <= frameLength + frameLengthHeaderSize) // can't read anymore - partial message
+                {
+                    return position;
+                }
+
+
+                // have at least one message inside buffer
+                var msg = buffer.Slice(frameLengthHeaderSize, frameLength);
+                decoded.Add(msg);
+                position = buffer.GetPosition(frameLength + frameLengthHeaderSize);
+                buffer = buffer.Slice(position);
             }
         }
     }
